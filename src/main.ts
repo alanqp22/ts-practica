7ed2883d6a2267df56222e77321e30f77543c149
Alan <alanquispe022@gmail.com>
2024-10-10 22:49:46 -0400

    contador con typescript

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,19 @@ personaUno.lastName = "Rios"
 console.log(personaUno);
 
 console.log(age, name);
+
+const $counter = document.getElementById("counter") as HTMLSpanElement;
+const $btnIncrement = document.getElementById("btnIncrement") as HTMLButtonElement;
+
+let counter: number = 0;
+
+const incrementar = () => {
+  counter++;
+  if($counter){
+    $counter.textContent = counter.toString();
+  }
+}
+
+if($btnIncrement){
+  $btnIncrement.addEventListener("click",incrementar);
+}
